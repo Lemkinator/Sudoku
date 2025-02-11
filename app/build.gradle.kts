62599a1056855c5778812e96b8d575dfa9d65a11
Leonard <leo@leonard-lemke.com>
2025-02-11 19:38:14 +0100

    release update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "de.lemke.sudoku"
         minSdk = 26
         targetSdk = 35
-        versionCode = 48
-        versionName = "3.3.5"
+        versionCode = 49
+        versionName = "3.3.6"
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
             arg("room.generateKotlin", "true")
