618435f136fdf072c422574389761805d2e8db5a
Leonard <leo@leonard-lemke.com>
2025-05-08 22:30:54 +0200

    Upgrade oneui-design dependency from 0.5.12 to 0.5.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     implementation("sesl.androidx.picker:picker-basic:1.0.17+1.0.17-sesl6+rev2")
     //SESL6(OneUI 6) Material Components + Design Lib + Icons
     implementation("sesl.com.google.android.material:material:1.12.0+1.0.23-sesl6+rev3")
-    implementation("io.github.tribalfs:oneui-design:0.5.12+oneui6")
+    implementation("io.github.tribalfs:oneui-design:0.5.13+oneui6")
     implementation("io.github.oneuiproject:icons:1.1.0")
 
     implementation("io.github.lemkinator:common-utils:0.8.2")
